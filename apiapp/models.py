5e293ba0e9fe1ad72aa960e683c8c0e25f303faf
webizfabulous <webizmoxx@gmail.com>
2019-06-29 13:05:55 +0545

    Added models

--- apiapp/models.py
+++ apiapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Api(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    size = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    
