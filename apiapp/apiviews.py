57696efd9aa8ffd673b8a461b66128ef37887839
webizfabulous <webizmoxx@gmail.com>
2019-06-29 16:35:37 +0545

    Added newest method

--- apiapp/apiviews.py
+++ apiapp/apiviews.py
@@ -1,7 +1,9 @@
 from  .models import Api
 from .serializers import ApiSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
+from rest_framework.response import Response
 
 class ApiViewset(viewsets.ModelViewSet):
     #list , create ,  retrieve , update , partial update , destroy
@@ -9,3 +11,11 @@ class ApiViewset(viewsets.ModelViewSet):
     queryset = Api.objects.all()
     serializer_class = ApiSerializer
 
+
+    #method=get
+    @action(methods=['get'] , detail=False) #detail=false means dont show this view on detail view
+    def newest(self,request):
+        newest = self.get_queryset().order_by('created').last() #gettting the last created model
+        serializer = self.get_serializer_class()(newest) #setting that model to serializer or say serializing the newest data
+        return Response(serializer.data) #returning jsonrespons
+    
